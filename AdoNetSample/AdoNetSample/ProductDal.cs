726d6be434e0653cde6048c32c3f72f8f104b16c
ibrahimTaskin <46849933+ibrahimTaskin@users.noreply.github.com>
2019-01-20 13:04:40 +0300

    Add files via upload

---
+++ AdoNetSample/AdoNetSample/ProductDal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetSample
+{
+    public class ProductDal
+    {
+        SqlConnection connection=new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+        public List<Product> GetAll()
+        {
+            if (connection.State==ConnectionState.Closed) //eğer bağlantı kapalıysa aç
+            {
+               connection.Open();
+            }
+
+            SqlCommand command=new SqlCommand("select * from Products",connection); //sql sorgusu
+
+            SqlDataReader reader = command.ExecuteReader(); // sorguyu çalıştırdık
+
+            List<Product> products=new List<Product>();
+
+            while (reader.Read()) //readerdan gelenler tek tek okunur
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+
+                products.Add(product); //listeye gelen nesneleri ekledik
+            }
+
+            reader.Close();
+            connection.Close();
+            return products; //listeyi geri döndürdük
+        }
+    }
+}
